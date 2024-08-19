f635f7f6c496cce016d1d853cebf2e4e765d9638
Mary Mhar Perez <marymharperez@gmail.com>
2024-08-20 00:37:58 +0800

    first commit

---
+++ App21/App21/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace App21
+{
+    public partial class MainPage : ContentPage
+    {
+        internal string SelectedTopping;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PlaceOrder_Clicked(object sender, EventArgs e)
+        {
+            double totalPrice = 0;
+
+            if (SmallRadioButton.IsChecked)
+                totalPrice += 300;
+            else if (MediumRadioButton.IsChecked)
+                totalPrice += 400;
+            else if (LargeRadioButton.IsChecked)
+                totalPrice += 500;
+
+            if (FrenchFriesCheckBox.IsChecked)
+                totalPrice += 100;
+            if (SideSaladCheckBox.IsChecked)
+                totalPrice += 150;
+            if (OnionRingsCheckBox.IsChecked)
+                totalPrice += 175;
+
+            string selectedTopping = ToppingsPicker.SelectedItem as string;
+            if (!string.IsNullOrEmpty(selectedTopping))
+            {
+                string[] splitTopping = selectedTopping.Split(new string[] { " - $" }, StringSplitOptions.None);
+                if (splitTopping.Length == 2)
+                {
+                    if (double.TryParse(splitTopping[1], out double toppingPrice))
+                        totalPrice += toppingPrice;
+                }
+            }
+
+            Navigation.PushAsync(new OrderSummaryPage(totalPrice));
+        }
+    }
+}
---
+++ App21/App21/Page1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+
+namespace App21
+{
+    public partial class OrderSummaryPage : ContentPage
+    {
+        public OrderSummaryPage(double totalPrice)
+        {
+            InitializeComponent();
+            PopulateOrderDetails(totalPrice);
+        }
+
+        private void PopulateOrderDetails(double totalPrice)
+        {
+            string orderDetails = "Selected Items:\n";
+
+            string selectedTopping = (BindingContext as MainPage)?.SelectedTopping;
+            if (!string.IsNullOrEmpty(selectedTopping))
+                orderDetails += $"Toppings: {selectedTopping}\n";
+
+            OrderDetailsLabel.Text = orderDetails;
+            TotalPriceLabel.Text = $"Total Price: ${totalPrice.ToString("F2")}";
+        }
+    }
+}
